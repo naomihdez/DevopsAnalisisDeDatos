19ae55ec4dfb865572ebbc154c163137d574b7ba
naomihdez <nao_nao04@hotmail.com>
2026-04-29 22:41:10 -0600

    Update docker-compose and airflow setup

--- files/init.sql
+++ files/init.sql
@@ -1,8 +1,6 @@
-CREATE DATABASE IF NOT EXISTS airflow_db;
-CREATE USER IF NOT EXISTS 'airflow_user'@'%' IDENTIFIED BY 'airflow_password';
 GRANT ALL PRIVILEGES ON airflow_db.* TO 'airflow_user'@'%';
-
 CREATE DATABASE IF NOT EXISTS sales_db;
+CREATE USER IF NOT EXISTS 'sales_user'@'%' IDENTIFIED BY 'sales_password';
 GRANT ALL PRIVILEGES ON sales_db.* TO 'sales_user'@'%';
 
 FLUSH PRIVILEGES;
